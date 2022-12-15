eb1ee18a21a70c738daa40e0bbac3f4fa5db5cdb
LauraTortosa <alutor9252@ieselcaminas.org>
2022-12-15 19:11:36 +0100

    .

--- src/Exercise11.java
+++ src/Exercise11.java
@@ -2,32 +2,86 @@ import java.util.Scanner;
 
 public class Exercise11 {
     public static void main(String[] args) {
-        char[][] board = new char[8][8];
-        char decirLetra;
-        int decirCol;
-
-        //tablero
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board.length; i++) {
-                board[i][j] = ' ';
+        final int NUM_ROWS = 8;
+        final int NUM_COLS = 8;
+        final int TOTAL_SHIPS = 10;
+        char board[][] = new char[NUM_ROWS][NUM_COLS];
+        char rowLetter;
+        int colNumber;
+        int shots = 0, sunkShips = 0;
+        boolean gameOver = false;
+        Scanner input = new Scanner(System.in);
+        for (int row = 0; row < board.length; row++) {
+            for (int col = 0; col < board[0].length; col++) {
+                board[row][col] = ' ';
             }
         }
 
-        //colocacion barcos
-        int cont = 0;
+        int shipCounter = 0;
+        int rowRand, colRand;
+        while (shipCounter < TOTAL_SHIPS) {
+            rowRand = (int) (Math.random() * NUM_ROWS);
+            colRand = (int) (Math.random() * NUM_COLS);
 
-        while (cont < 10) {
-            int x = (int) (Math.random() * 10); //derecha-izquierda
-            int y = (int) (Math.random() * 10); //arriba-abajo
+            if (board[rowRand][colRand] == ' ') {
+                board[rowRand][colRand] = 'S';
+                shipCounter ++;
+            }
         }
+        System.out.println("-----------------");
+        System.out.println("SHOTS: " + shots);
+        System.out.println("SUNK SHIPS: " + sunkShips);
+        do {
+            System.out.print("Enter row (letter):");
+            rowLetter = input.next().toUpperCase().charAt(0);
+            System.out.print("Enter column (number):");
+            colNumber = input.nextInt() - 1;
 
-        //decir coordenadas (letra y numero)
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Escribe una letra: ");
-        decirLetra = scan.next().charAt(0);
-        System.out.println("Escribe una columna: ");
-        decirCol = scan.nextInt();
+            int rowNumber = rowLetter - 'A';
 
+            if (rowNumber < 0 || rowNumber >= NUM_ROWS ||
+                    colNumber < 0 || colNumber >= NUM_COLS) {
+                continue;
+            }
+            if (board[rowNumber][colNumber] == ' ') {
 
+                board[rowNumber][colNumber] = 'O';
+            } else {
+                if (board[rowNumber][colNumber] == 'S') {
+
+                    board[rowNumber][colNumber] = 'X';
+                    sunkShips++;
+                    if (sunkShips == TOTAL_SHIPS) {
+                        gameOver = true;
+                    }
+                }
+            }
+            shots++;
+
+            System.out.println("SHOTS: " + shots);
+            System.out.println("SUNK SHIPS " + sunkShips + "\n");
+
+            System.out.print(" ");
+            for (int col = 1; col <= board[0].length; col++) {
+                System.out.print(col + " ");
+            }
+            System.out.println();
+            char letterBoard = 'A';
+            for (int row = 0; row < board.length; row++) {
+                System.out.print(letterBoard + " ");
+                for (int col = 0; col < board[0].length; col++) {
+                    if (board[row][col] == 'S') {
+
+                        System.out.print(" ");
+                    } else {
+                        System.out.print(board[row][col] + " ");
+                    }
+                }
+                System.out.println();
+                letterBoard++;
+            }
+        } while (!gameOver);
+        System.out.println("YOU WIN. ALL SHIPS SUNK.");
     }
 }
+
